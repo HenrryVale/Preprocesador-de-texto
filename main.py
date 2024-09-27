12d176128aa1b963e0c6e9114d728d561b97420b
HenrryVale <henrryvalenzuelaperez@gmail.com>
2024-09-27 01:13:15 -0500

    Subida de fuente de preprocesador de textos
    Subida de fuente de preprocesador de textos

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+def validar_solo_letras(cadena):
+    letras_permitidas = "áéíóúÁÉÍÓÚñÑabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ \n"
+    resultado = ''.join(c for c in cadena if c in letras_permitidas)
+    return resultado
+
+def validar_txt(file):
+    with open(file, 'r', encoding="UTF-8") as file:
+        #Poner a minusculas
+        contenido = file.read().lower()
+        #valida solo letras
+        nuevo_contenido = validar_solo_letras(contenido)
+        #Tokenizar 
+        nuevo_contenido_tokenizado = nuevo_contenido.split()
+        return nuevo_contenido_tokenizado #print(nuevo_contenido_tokenizado)
+
+texto_a_procesar = validar_txt('raw-text.txt')
+stop_words = validar_txt('stop-words.txt')
+#Filtar stop-words
+resultado = [token for token in texto_a_procesar if token not in stop_words]
+print(resultado)
